61207288767e4e83065853845e07aa4b494d78af
itsoladayo <oladayogn@gmail.com>
2020-07-18 15:19:04 -0400

    testing game

--- www/js/MasterMind.js
+++ www/js/MasterMind.js
@@ -223,6 +223,7 @@ function doStart() {
           this.hiddenList[i] = 'white';
         }
       }
+      alert('hidden list');
     } // generateHiddenList
 
     this.drawTurns = drawTurns;
@@ -501,26 +502,26 @@ function doStart() {
         //   }
       }
 
-      for (var i = 0; i < this.currentPegs.length; i++) {
-        if (this.currentPegs[i] === 'red') {
-          this.currentPegs[i] = 0;
-        } else if (this.currentPegs[i] === 'white') {
-          this.currentPegs[i] = 1;
-        } else {
-          this.currentPegs[i] = 2;
-        }
-      }
-      this.currentPegs.sort();
+      //   for (var i = 0; i < this.currentPegs.length; i++) {
+      //     if (this.currentPegs[i] === 'red') {
+      //       this.currentPegs[i] = 0;
+      //     } else if (this.currentPegs[i] === 'white') {
+      //       this.currentPegs[i] = 1;
+      //     } else {
+      //       this.currentPegs[i] = 2;
+      //     }
+      //   }
+      //   this.currentPegs.sort();
 
-      for (var i = 0; i < this.currentPegs.length; i++) {
-        if (this.currentPegs[i] === 0) {
-          this.currentPegs[i] = 'red';
-        } else if (this.currentPegs[i] === 1) {
-          this.currentPegs[i] = 'white';
-        } else {
-          this.currentPegs[i] = 'black';
-        }
-      }
+      //   for (var i = 0; i < this.currentPegs.length; i++) {
+      //     if (this.currentPegs[i] === 0) {
+      //       this.currentPegs[i] = 'red';
+      //     } else if (this.currentPegs[i] === 1) {
+      //       this.currentPegs[i] = 'white';
+      //     } else {
+      //       this.currentPegs[i] = 'black';
+      //     }
+      //   }
 
       this.boardPegs[this.currentRow] = this.currentPegs;
 
