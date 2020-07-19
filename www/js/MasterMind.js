6c46a1a4a3592831b82b8bd5aecd39b7accdff9b
itsoladayo <oladayogn@gmail.com>
2020-07-18 21:09:05 -0400

    fixing bugs when there's a red peg

--- www/js/MasterMind.js
+++ www/js/MasterMind.js
@@ -408,10 +408,10 @@ function doStart() {
         var pegCounter = 0;
         var counter = 0;
         if (
-          (this.hiddenList.includes(c) &&
-            this.currentPegs[i] != 'red' &&
-            this.hiddenList[i + 1] != c) ||
-          this.hiddenList[i + 2] != c
+          this.hiddenList.includes(c) &&
+          this.currentPegs[i] != 'red'
+          //     this.hiddenList[i + 1] != c) ||
+          //   this.hiddenList[i + 2] != c
         ) {
           //Check for one of the hidden colors
           //     // if (this.hiddenList[k] === c && k < i + 2)
