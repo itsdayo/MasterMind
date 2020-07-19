f1191119fcdc1b624bfcff085fbadbb1a2780f69
itsoladayo <oladayogn@gmail.com>
2020-07-19 14:42:36 -0400

    solved the previous bug with pegs

--- www/js/MasterMind.js
+++ www/js/MasterMind.js
@@ -223,7 +223,6 @@ function doStart() {
           this.hiddenList[i] = 'white';
         }
       }
-      this.hiddenList = ['white', 'red', 'green', 'red'];
 
       console.log(this.hiddenList);
     } // generateHiddenList
