be2da560c0459c39e1d985d3b9cb5eb3c89666d2
itsoladayo <oladayogn@gmail.com>
2020-07-18 21:49:13 -0400

    fixing a bug spotted

--- www/js/MasterMind.js
+++ www/js/MasterMind.js
@@ -441,8 +441,8 @@ function doStart() {
                 if (
                   (pegCounter < counter && this.hiddenList[i + 1] != c) ||
                   this.hiddenList[i + 2] != c ||
-                  this.hiddenList[i - 2] ||
-                  this.hiddenList[i - 1]
+                  this.hiddenList[i - 2] != c ||
+                  this.hiddenList[i - 1] != c
                 ) {
                   this.currentPegs[i] = 'white';
                   pegCounter = 0;
