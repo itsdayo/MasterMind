04a7372ead4cb87ab214fa8e01b96c29f7d0153b
Oladayo Ogundipe <oladayoogundipe@oladayos-mbp.home>
2021-01-14 18:38:35 -0800

    buttons fixed when clicked

--- www/js/MasterMind.js
+++ www/js/MasterMind.js
@@ -51,7 +51,7 @@ function plotCircle(row, col, fillColor) {
   g2D.fillStyle = fillColor;
   g2D.fill();
   g2D.lineWidth = 5;
-  g2D.strokeStyle = '#003300';
+  g2D.strokeStyle = "#003300";
 } //plotCircle
 
 function setPenColor(color) {
@@ -81,7 +81,7 @@ function plotSquare(cX, cY, size) {
 function drawColorButton(cX, cY, size, fillColor) {
   setFillColor(fillColor);
   plotSquare(cX, cY, size);
-  setPenColor('black');
+  setPenColor("black");
   setPenSize(3);
   drawSquare(cX, cY, size);
 }
@@ -111,46 +111,47 @@ function inUndoButton(x, y) {
   //   if (Math.abs(554 - x) < 48 && Math.abs(undoY + 22 - y) < 24) {
   //     alert('undo');
   //   }
-  return Math.abs(554 - x) < 47 && Math.abs(undoY + 22 - y) < 25;
+  return Math.abs(x - 455) < 47 && Math.abs(y - 60) < 25;
 } //inUndoButton
 
 function inBlueColorButton(x, y) {
-  if (Math.abs(x - 218) < 23 && Math.abs(y - 80) < 25) {
+  //if (Math.abs(x - 218) < 23 && Math.abs(y - 80) < 25) {
+  if (Math.abs(x - 190) < 15 && Math.abs(y - 60) < 25) {
     return true;
   } else {
     return false;
   }
 }
 function inRedColorButton(x, y) {
-  if (Math.abs(x - 273) < 23 && Math.abs(y - 80) < 25) {
+  if (Math.abs(x - 230) < 15 && Math.abs(y - 60) < 25) {
     return true;
   } else {
     return false;
   }
 }
 function inGreenColorButton(x, y) {
-  if (Math.abs(x - 327) < 23 && Math.abs(y - 80) < 25) {
+  if (Math.abs(x - 270) < 15 && Math.abs(y - 60) < 25) {
     return true;
   } else {
     return false;
   }
 }
 function inYellowColorButton(x, y) {
-  if (Math.abs(x - 382) < 23 && Math.abs(y - 80) < 25) {
+  if (Math.abs(x - 314) < 15 && Math.abs(y - 60) < 25) {
     return true;
   } else {
     return false;
   }
 }
 function inPurpleColorButton(x, y) {
-  if (Math.abs(x - 437) < 23 && Math.abs(y - 80) < 25) {
+  if (Math.abs(x - 370) < 15 && Math.abs(y - 60) < 25) {
     return true;
   } else {
     return false;
   }
 }
 function inWhiteColorButton(x, y) {
-  if (Math.abs(x - 492) < 23 && Math.abs(y - 80) < 20) {
+  if (Math.abs(x - 400) < 15 && Math.abs(y - 60) < 20) {
     return true;
   } else {
     return false;
@@ -161,12 +162,12 @@ function drawButtons() {
   g2D.drawImage(resetButton, 30, 50, 120, 60);
   g2D.drawImage(undoButton, undoX, undoY, 120, 60);
 
-  drawColorButton(240, 80, 50, 'blue');
-  drawColorButton(300, 80, 50, 'red');
-  drawColorButton(360, 80, 50, 'green');
-  drawColorButton(420, 80, 50, 'yellow');
-  drawColorButton(480, 80, 50, 'purple');
-  drawColorButton(540, 80, 50, 'white');
+  drawColorButton(240, 80, 50, "blue");
+  drawColorButton(300, 80, 50, "red");
+  drawColorButton(360, 80, 50, "green");
+  drawColorButton(420, 80, 50, "yellow");
+  drawColorButton(480, 80, 50, "purple");
+  drawColorButton(540, 80, 50, "white");
   game.drawTurns(10);
 } //drawButtons
 
@@ -176,26 +177,25 @@ function doPaintCanvas() {
 } // doPaintCanvas
 
 function doStart() {
-  backgroundImage.src = 'images/woodback.png';
-  undoButton.src = 'images/undo.png';
-  resetButton.src = 'images/reset.png';
-  Turns.src = 'images/turnRec.png';
+  backgroundImage.src = "images/woodback.png";
+  undoButton.src = "images/undo.png";
+  resetButton.src = "images/reset.png";
+  Turns.src = "images/turnRec.png";
 
   function MasterMindGame(maxTurns) {
     this.currentRow = 0;
-    this.currentRowColors = ['', '', '', ''];
+    this.currentRowColors = ["", "", "", ""];
     this.currentColumn = 0;
     this.maxTurns = maxTurns;
     this.board = new Array();
 
-
     this.rowBoard = new Array();
-    this.colors = ['red', 'green', 'blue', 'yellow', 'purple', 'white'];
+    this.colors = ["red", "green", "blue", "yellow", "purple", "white"];
     this.hiddenList = [0, 0, 0, 0];
-    this.currentPegs = ['black', 'black', 'black', 'black'];
+    this.currentPegs = ["black", "black", "black", "black"];
     this.boardPegs = new Array();
     this.drawnRow = 0;
-    this.rowColors = ['', '', '', ''];
+    this.rowColors = ["", "", "", ""];
 
     this.generateHiddenList = generateHiddenList;
     function generateHiddenList() {
@@ -207,22 +207,22 @@ function doStart() {
 
       for (i = 0; i < 4; i++) {
         if (this.hiddenList[i] === 0) {
-          this.hiddenList[i] = 'blue';
+          this.hiddenList[i] = "blue";
         }
         if (this.hiddenList[i] === 1) {
-          this.hiddenList[i] = 'red';
+          this.hiddenList[i] = "red";
         }
         if (this.hiddenList[i] === 2) {
-          this.hiddenList[i] = 'green';
+          this.hiddenList[i] = "green";
         }
         if (this.hiddenList[i] === 3) {
-          this.hiddenList[i] = 'yellow';
+          this.hiddenList[i] = "yellow";
         }
         if (this.hiddenList[i] === 4) {
-          this.hiddenList[i] = 'purple';
+          this.hiddenList[i] = "purple";
         }
         if (this.hiddenList[i] === 5) {
-          this.hiddenList[i] = 'white';
+          this.hiddenList[i] = "white";
         }
       }
 
@@ -253,25 +253,23 @@ function doStart() {
 
         this.currentColumn = 0;
         this.currentRow = this.currentRow + 1;
-        this.currentRowColors = ['', '', '', ''];
+        this.currentRowColors = ["", "", "", ""];
         this.board[this.currentRow] = this.currentRowColors;
       }
     } //setCellColor
 
     this.drawRow = drawRow;
     function drawRow(row) {
-      if(this.board[row]!=undefined){
-      this.rowColors = this.board[row];
+      if (this.board[row] != undefined) {
+        this.rowColors = this.board[row];
       }
-      
-      for (var i = 0; i < 4; i++) {
 
+      for (var i = 0; i < 4; i++) {
         var c = this.rowColors[i];
-        if (c != '') {
+        if (c != "") {
           setFillColor(c);
           drawDisk(140 + 167 * i, 185 + 45 * row, 10);
         }
-
       }
 
       //   var pegColors = this.boardPegs[row];
@@ -288,13 +286,13 @@ function doStart() {
     this.drawUndoRow = drawUndoRow;
     function drawUndoRow() {
       var newRowColors = this.rowColors;
-      newRowColors[this.currentColumn - 1] = '';
+      newRowColors[this.currentColumn - 1] = "";
       if ((this.currentColumn = 0)) {
         return;
       }
       for (var i = 0; i < 4; i++) {
         var c = newRowColors[i];
-        if (c != '') {
+        if (c != "") {
           setFillColor(c);
           drawDisk(140 + 167 * i, 185 + 62 * this.drawnRow, 10);
         }
@@ -305,7 +303,7 @@ function doStart() {
       this.board[this.currentRow] = this.currentRowColors;
       this.currentRow = 0;
       this.currentColumn = 0;
-      this.currentRowColors = ['', '', '', ''];
+      this.currentRowColors = ["", "", "", ""];
       this.board = [];
       this.generateHiddenList();
       // this.drawRow(0);
@@ -319,20 +317,20 @@ function doStart() {
         var c = this.currentRowColors[i];
 
         if (c === this.hiddenList[i]) {
-          this.currentPegs[i] = 'red';
+          this.currentPegs[i] = "red";
         } else {
-          this.currentPegs[i] = 'black';
+          this.currentPegs[i] = "black";
         }
         var currentCounter = 0;
         var matchCounter = 0;
         var hiddCounter = 0;
         var backHiddenCounter = 0;
         var forwHiddenCounter = 0;
-        if (this.hiddenList.includes(c) && this.currentPegs[i] != 'red') {
+        if (this.hiddenList.includes(c) && this.currentPegs[i] != "red") {
           //Check for one of the hidden colors
           //     // if (this.hiddenList[k] === c && k < i + 2)
 
-          this.currentPegs[i] = 'white';
+          this.currentPegs[i] = "white";
 
           for (var k = i - 1; k >= 0; k--) {
             if (this.currentRowColors[k] === c) {
@@ -362,82 +360,81 @@ function doStart() {
             }
 
             if (matchCounter >= counter) {
-              this.currentPegs[i] = 'white';
+              this.currentPegs[i] = "white";
             }
           }
           if (i === 0) {
-            if (this.currentPegs[i] != 'red' && counter >= currentCounter) {
-              this.currentPegs[i] = 'white';
+            if (this.currentPegs[i] != "red" && counter >= currentCounter) {
+              this.currentPegs[i] = "white";
             }
 
             if (hiddCounter <= currentCounter) {
               //  console.log(matchCounter, counter, 'third column');
-              this.currentPegs[i] = 'black';
+              this.currentPegs[i] = "black";
             }
           }
           if (i === 1) {
             if (matchCounter === 1 && hiddCounter < 1)
               //  console.log(matchCounter, counter, 'third column');
-              this.currentPegs[i] = 'black';
+              this.currentPegs[i] = "black";
 
             if (hiddCounter <= currentCounter)
               //  console.log(matchCounter, counter, 'third column');
-              this.currentPegs[i] = 'black';
+              this.currentPegs[i] = "black";
           }
 
           if (i === 2) {
             if (matchCounter === 2 && hiddCounter <= 1)
               //  console.log(matchCounter, counter, 'third column');
-              this.currentPegs[i] = 'black';
+              this.currentPegs[i] = "black";
 
             if (hiddCounter <= currentCounter) {
               //  console.log(matchCounter, counter, 'third column');
-              this.currentPegs[i] = 'black';
+              this.currentPegs[i] = "black";
             }
             if (
-              this.currentPegs[i] != 'red' &&
+              this.currentPegs[i] != "red" &&
               forwHiddenCounter != 1 &&
               matchCounter >= hiddCounter
             ) {
-              this.currentPegs[i] = 'black';
+              this.currentPegs[i] = "black";
             }
           }
           if (i === 3) {
             console.log(backHiddenCounter, matchCounter, hiddCounter);
             if (matchCounter === 1 && backHiddenCounter > matchCounter) {
               // console.log(matchCounter, counter, 'third column');
-              this.currentPegs[i] = 'white';
+              this.currentPegs[i] = "white";
             }
 
             if (matchCounter === 2 && backHiddenCounter > matchCounter) {
               //    console.log(matchCounter, counter, 'third column');
-              this.currentPegs[i] = 'white';
+              this.currentPegs[i] = "white";
             }
             if (matchCounter === 2 && hiddCounter > matchCounter) {
               //    console.log(matchCounter, counter, 'third column');
-              this.currentPegs[i] = 'white';
+              this.currentPegs[i] = "white";
             }
             if (backHiddenCounter <= matchCounter) {
-             
-              this.currentPegs[i] = 'black';
+              this.currentPegs[i] = "black";
             }
             //else {
             //  this.currentPegs[i] = 'orange';
             // }
             if (matchCounter === 3 && backHiddenCounter <= matchCounter) {
               //    console.log(matchCounter, counter, 'third column');
-              this.currentPegs[i] = 'black';
+              this.currentPegs[i] = "black";
             }
           }
         } else {
-          this.currentPegs[i] === 'black';
+          this.currentPegs[i] === "black";
         }
       }
 
       for (var i = 0; i < this.currentPegs.length; i++) {
-        if (this.currentPegs[i] === 'red') {
+        if (this.currentPegs[i] === "red") {
           this.currentPegs[i] = 0;
-        } else if (this.currentPegs[i] === 'white') {
+        } else if (this.currentPegs[i] === "white") {
           this.currentPegs[i] = 1;
         } else {
           this.currentPegs[i] = 2;
@@ -447,11 +444,11 @@ function doStart() {
 
       for (var i = 0; i < this.currentPegs.length; i++) {
         if (this.currentPegs[i] === 0) {
-          this.currentPegs[i] = 'red';
+          this.currentPegs[i] = "red";
         } else if (this.currentPegs[i] === 1) {
-          this.currentPegs[i] = 'white';
+          this.currentPegs[i] = "white";
         } else {
-          this.currentPegs[i] = 'black';
+          this.currentPegs[i] = "black";
         }
       }
 
@@ -468,15 +465,15 @@ function doStart() {
       for (i = 1; i < pegs[0].length + 1; i++) {
         plotCircle(n, i, pegColors[i - 1]);
       }
-      if (pegColors[3] === 'red') {
-        console.log('won');
-        alert('You win! the colors were' + ' ' + solution);
+      if (pegColors[3] === "red") {
+        console.log("won");
+        alert("You win! the colors were" + " " + solution);
         this.resetColors();
         return;
       }
       if (n === 10) {
-        console.log('lost');
-        alert('Sorry you lose! The colors were' + ' ' + solution);
+        console.log("lost");
+        alert("Sorry you lose! The colors were" + " " + solution);
         this.resetColors();
       }
     } //drawPegs
@@ -485,30 +482,30 @@ function doStart() {
   game = new MasterMindGame(10);
   game.generateHiddenList();
 
-  theCanvas = document.getElementById('myCanvas');
-  theCanvasOffset = $('#myCanvas').offset();
-  g2D = theCanvas.getContext('2d');
-  $('#myCanvas').attr('style', 'border: 7px solid;');
+  theCanvas = document.getElementById("myCanvas");
+  theCanvasOffset = $("#myCanvas").offset();
+  g2D = theCanvas.getContext("2d");
+  $("#myCanvas").attr("style", "border: 7px solid;");
 
-  $('#myCanvas').mouseenter(function (e) {
+  $("#myCanvas").mouseenter(function (e) {
     getMouseXY(e);
   });
 
-  $('#myCanvas').mousedown(function (e) {
+  $("#myCanvas").mousedown(function (e) {
     getMouseXY(e);
 
     if (inBlueColorButton(mX, mY)) {
-      game.setCellColor('blue');
+      game.setCellColor("blue");
     } else if (inRedColorButton(mX, mY)) {
-      game.setCellColor('red');
+      game.setCellColor("red");
     } else if (inGreenColorButton(mX, mY)) {
-      game.setCellColor('green');
+      game.setCellColor("green");
     } else if (inYellowColorButton(mX, mY)) {
-      game.setCellColor('yellow');
+      game.setCellColor("yellow");
     } else if (inPurpleColorButton(mX, mY)) {
-      game.setCellColor('purple');
+      game.setCellColor("purple");
     } else if (inWhiteColorButton(mX, mY)) {
-      game.setCellColor('white');
+      game.setCellColor("white");
       // game.checkGuess();
     } else if (inUndoButton(mX, mY)) {
       game.drawUndoRow();
@@ -523,11 +520,11 @@ function doStart() {
     //checkGuess();
   }); //mousedown
 
-  $('#myCanvas').mousemove(function (e) {
+  $("#myCanvas").mousemove(function (e) {
     getMouseXY(e);
   });
 
-  $('#myCanvas').mouseup(function (e) { });
+  $("#myCanvas").mouseup(function (e) {});
 
   backgroundImage.onload = function () {
     doPaintCanvas();
